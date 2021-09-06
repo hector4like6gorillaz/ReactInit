928ddcb90825b1be64ccfe171f84f960c799ac00
hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2021-09-06 16:43:43 -0500

    Router ready v2

--- src/App/components/Routes.tsx
+++ src/App/components/Routes.tsx
@@ -1,6 +1,7 @@
 
 import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "../../Pages/Home/Home";
+import Page404 from "../../Pages/Page404/Page404";
 
 
 const Routes = () => {
@@ -10,16 +11,17 @@ const Routes = () => {
         <>
             <Switch>
                 {/* Home */}
-                <Route exact path="/home">
+                <Route exact path="/">
                     {senti ? <Home /> : <Redirect to="/404" />}
                 </Route>
-
 
                 {/*404 not found*/}
                 <Route exact path="/404">
                     {<h1>No se encuentra la pagina</h1>}
                 </Route>
                 <Redirect to="/404" />
+
+                <Route component={Page404} />
 
             </Switch>
         </>
